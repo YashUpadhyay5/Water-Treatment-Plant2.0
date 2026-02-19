770cd0a269e3ac9614b17100e6cbbf2896e1c265
YashUpadhyay5 <yashupdhyay486@gmail.com>
2026-02-19 23:13:07 +0530

    Initial fullstack upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import userRoutes from './routes/user.js';
 import adminRoutes from './routes/admin.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
